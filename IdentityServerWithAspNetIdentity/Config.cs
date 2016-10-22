aa3beab48f29a992ff3eadbe0165e2af6fdb3496
Mário Monteiro <mail@koakh.com>
2016-10-22 20:08:53 +0100

    Commit

---
+++ IdentityServerWithAspNetIdentity/Config.cs
@@ -0,0 +1,51 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerWithAspNetIdentity
+{
+    public class Config
+    {
+
+        //Defining the scope
+        //http://identityserver4.readthedocs.io/en/dev/quickstarts/1_client_credentials.html#defining-the-scope
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API"
+                }
+            };
+        }
+
+        //Defining the client
+        //http://identityserver4.readthedocs.io/en/dev/quickstarts/1_client_credentials.html#defining-the-client
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
---
+++ NOTES.md
@@ -0,0 +1,6 @@
+echo "# NetCoreAspIdentityServer4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/koakh/NetCoreAspIdentityServer4.git
+git push -u origin master
